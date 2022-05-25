632fb77daaf15a620c47f93f423136186aeec7f6
Lentsu <Lenni.Toikkanen@aalto.fi>
2022-05-25 16:44:54 +0300

    updated ino file

--- project_peilipyyhin/project_peilipyyhin.ino
+++ project_peilipyyhin/project_peilipyyhin.ino
@@ -1,37 +1,50 @@
-#include <Wire.h>
-
 /*Example sketch to control a stepper motor with A4988/DRV8825 stepper motor driver and Arduino without a library. More info: https://www.makerguides.com */
-
+#include <Wire.h>
 #include <math.h>
 #include "DHT20.h"
-//#include "wire.h"
+
+// Uncomment for debug
+//#define DEBUG
+
+// Offset for calibration if DEBUG is defined
+#ifdef DEBUG
+  #define hum_offset 100
+  #define temp_offset 100
+#else
+  #define hum_offset 0
+  #define temp_offset 0
+#endif
 
 // Define stepper motor connections and steps per revolution:
 #define dirPin 2
 #define stepPin 3
 
-static const uint8_t dataPin = 5;
-static const uint8_t clockPin = 6;
-
-// Globals for data reading
+// Data reading
 #define clockSpeed 400000
 #define readDelay 1000
 
-// Globals for motor stuff
+// Globals for stepper motor
+//  Steps and direction
 #define stepsPerRevolution 50
-#define stepDelay 6000
+#define DIRECTION 1
+//  wipe delays
+#define stepDelay 12000
+#define delayAmount 1000
+#define middleDelay 400
 
 // Define the DHT20 and wire
 DHT20 DHT;
 
 // Data value holders
-float hum, temp, dew;
-float a = 17.27;
-float b = 237.7;
+float hum, temp;
+bool conditional;
 
-// Offset for calibration
-static const int hum_offset = 0;
-static const int temp_offset = 0;
+// The minimum environment condition for a wipe
+static const int hum_min = 50;    // Wipe also whenever hum_min + 30
+static const int temp_min = 35;
+
+// Function to wipe once
+void wipe();
 
 void setup() {
   
@@ -41,8 +54,8 @@ void setup() {
   
   // Setup the DHT
   Wire.begin();
-  DHT.begin();
   Wire.setClock(400000);
+  DHT.begin();
   Serial.begin(115200);
 
   // Calibrating
@@ -81,52 +94,69 @@ void loop() {
     hum = DHT.getHumidity();
     temp = DHT.getTemperature();
 
-    // Calculate the dew point
-    dew = (b*(a*temp)/(b+temp)+log(hum))/(a-(a*temp)/(b+temp)+log(hum));
-
-    bool conditional = temp < dew;
+    // Calculate the conditional
+    conditional = ((hum >= hum_min && temp >= temp_min) || (hum >= (hum_min + 10)));
     
     // Print the data to serial
     Serial.print("DHT20, \t");
     Serial.print(hum, 1);
+    Serial.print("/("); Serial.print(hum_min, 1); Serial.print(")%");
     Serial.print(",\t");
     Serial.print(temp, 1);
+    Serial.print("/("); Serial.print(temp_min, 1); Serial.print(")C");
     Serial.print(",\t");
-    Serial.print(dew, 1);
-    Serial.print(",\t");
-    Serial.print("condition met? ");
-    Serial.print(conditional ? "Yup" : "Nup");
+    Serial.print("Wipe: \t");
+    Serial.print(conditional ? "Yes" : "No");
     Serial.print("\n");
   }
-
   
-  
-  // Set the spinning direction clockwise:
-  digitalWrite(dirPin, HIGH);
+  // Wipe once if condition is met
+  if (conditional) 
+    wipe();
 
-  // Spin the stepper motor 1 revolution slowly:
-  for (int i = 0; i < stepsPerRevolution; i++) {
-    // These four lines result in 1 step:
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(stepDelay);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(stepDelay);
+  // Delay for delay amount
+  delay(delayAmount);
+    
+}
+
+// Move the motor a single step to direction
+static void step_(int sPin, int dPin, int d) {
+
+  // Set direction
+  digitalWrite(dirPin, d);
+  // Step
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(stepDelay/2);
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(stepDelay/2);
+
+}
+
+// Move the motor n steps
+static void n_step_(int sPin, int dPin, int d, int n) {
+  for (int i = 0; i < stepsPerRevolution; ++i) {
+    step_(sPin, dPin, d);
   }
+}
 
-  delay(1000);
+// Wipe once
+void wipe() {
+
+  // Move the motor half a revolution TO direction
+  n_step_(stepPin, dirPin, DIRECTION, stepsPerRevolution/2);
+
+  // Delay for middleDelay
+  delay(middleDelay);
   
-  // Set the spinning direction counterclockwise:
-  digitalWrite(dirPin, LOW);
+  // Move the motor a revolution FROM direction
+  n_step_(stepPin, dirPin, !DIRECTION, stepsPerRevolution);
 
-  // Spin the stepper motor 1 revolution quickly:
-  for (int i = 0; i < stepsPerRevolution; i++) {
-    // These four lines result in 1 step:
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(stepDelay);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(stepDelay);
-  }
+  // Delay for middleDelay
+  delay(middleDelay);
 
-  delay(1000);
-  
+  // Move back to the middle
+  n_step_(stepPin, dirPin, DIRECTION, stepsPerRevolution/2);
+
+  // Delay for middleDelay
+  delay(middleDelay);
 }
